b1a5e5f0440def0b2cce9452d598529aaed51939
George Sgouros <sgouros@sgouros.gr>
2016-08-17 10:58:26 +0300

    2	να σχεδιάσεις το κύριο μενού με bootstrap	οκ 6	i18	οκ

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem "haml-rails", "~> 0.9"
 
-gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'bootstrap-sass', '3.3.6'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # automated activation of lins in bootstrap navbar
+  # thanks to Wawa Loo http://stackoverflow.com/questions/17481812/dynamically-add-active-class-to-bootstrap-li-in-rails
+  def link_to_auto_active(body, url, html_options = {})
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url, html_options
+    end
+  end
+
+
 end
