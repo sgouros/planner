30ae2af208eb98f28ace25f8d98a38127779288a
George Sgouros <sgouros@sgouros.gr>
2016-09-29 10:28:27 +0300

    changed on 2016.Σεπ.29_10.28 by GS

--- 01/planner_stop_nm.sh
+++ 01/planner_stop_nm.sh
@@ -1,11 +1,6 @@
 DATE="$(date +"%Y.%b.%d_%H.%M")"
-cd ~/code/RoR/planner
-echo -n "-- Planner stop session -----------\n"
-echo -n "1. git status\n"
+
 git status -s
-echo -n "2. git add .\n"
 git add .
-echo -n '3. git commit with prefixed message\n'
 git commit -m "changed on $DATE by GS"
-echo -n "4. git push origin master\n"
 git push origin master
